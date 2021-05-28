4e2d96dc3a0c36f01f12a5a08e6fad0108e96115
Guillaume Taquet Gasperini <perso@aerion.me>
2021-05-28 09:45:50 +0200

    popup-calendar: add arguments to override hardcoded values

--- polybar-scripts/popup-calendar/popup-calendar.sh
+++ polybar-scripts/popup-calendar/popup-calendar.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
-BAR_HEIGHT=22  # polybar height
-BORDER_SIZE=1  # border size from your wm settings
-YAD_WIDTH=222  # 222 is minimum possible value
-YAD_HEIGHT=193 # 193 is minimum possible value
-DATE="$(date +"%a %d %H:%M")"
+DATE_FORMAT=${1:-"%a %d %H:%M"}
+BAR_HEIGHT=${2:-22}  # polybar height
+BORDER_SIZE=${3:-1}  # border size from your wm settings
+YAD_WIDTH=${4:-222}  # 222 is minimum possible value
+YAD_HEIGHT=${5:-193} # 193 is minimum possible value
+DATE="$(date +"$DATE_FORMAT")"
 
 case "$1" in
 --popup)
