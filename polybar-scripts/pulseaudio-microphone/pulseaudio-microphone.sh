e465c0886e01ef87e364bc4bba478071eb193915
Guillaume Taquet Gasperini <perso@aerion.me>
2021-05-28 09:45:50 +0200

    microphone: use custom icons

--- polybar-scripts/pulseaudio-microphone/pulseaudio-microphone.sh
+++ polybar-scripts/pulseaudio-microphone/pulseaudio-microphone.sh
@@ -1,12 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 status() {
   MUTED=$(pacmd list-sources | awk '/\*/,EOF {print}' | awk '/muted/ {print $2; exit}')
 
   if [ "$MUTED" = "yes" ]; then
-    echo "muted"
+    echo "%{R}  "
   else
-    pacmd list-sources | grep "\* index:" -A 7 | grep volume | awk -F/ '{print $2}' | tr -d ' '  
+    echo -n " "
+    pacmd list-sources | grep "\* index:" -A 7 | grep volume | awk -F/ '{print $2}' | tr -d ' '
   fi
 }
 
